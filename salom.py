a73ba47d4fc40c48c78777475db289cb46d97bb1
otabek-a <otabekabdurasulov245@gmail.com>
2024-12-02 13:25:03 +0500

    qwerty

--- salom.py
+++ salom.py
@@ -1,12 +1,13 @@
 from telegram import Update
 from telegram.ext import Application, MessageHandler, filters, CallbackContext
-
+import os
+env_var=os.environ
 # Bot tokenini o'zgartiring
-TOKEN = '7386230325:AAFxpvPEXJCM_rIVaKFY45tW5ndhU7RPZw8'
+TOKEN = env_var['TOKEN']
 
 async def handle_message(update: Update, context: CallbackContext):
     # Yuborilgan xabarni o'zingizga yo'naltirish
-    chat_id = 6723268646  # Sizning Telegram IDingiz
+    chat_id = env_var['E']  # Sizning Telegram IDingiz
     await context.bot.forward_message(chat_id=chat_id, 
                                       from_chat_id=update.message.chat_id, 
                                       message_id=update.message.message_id)
